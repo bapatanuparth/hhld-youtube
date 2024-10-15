1550b255dd27ad68cbedf136bfa3266f2cff3cfe
bapatanuparth <67226812+bapatanuparth@users.noreply.github.com>
2024-10-15 11:14:59 -0400

    OAuth flow

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,10 @@
 import Room from "./pages/Room";
+import Auth from "./pages/Auth";
 
 export default function Home() {
   return (
     <div>
-      <Room></Room>
+      <Auth></Auth>
     </div>
   );
 }
