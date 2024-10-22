67a7414285cdcb32fc0996eec44d1ab12e3ac3ac
bapatanuparth <67226812+bapatanuparth@users.noreply.github.com>
2024-10-22 10:07:06 -0400

    watch-service to view video

---
+++ watch_service/routes/watch.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import watchVideo from "../controllers/watch.controller.js";
+
+const router = express.Router();
+
+router.get("/", watchVideo);
+
+export default router;
