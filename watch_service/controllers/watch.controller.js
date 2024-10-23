a2490a28b921762d0deab60fd7c33f3e6e68c304
bapatanuparth <67226812+bapatanuparth@users.noreply.github.com>
2024-10-23 11:03:45 -0400

    watch-service-debug

--- watch_service/controllers/watch.controller.js
+++ watch_service/controllers/watch.controller.js
@@ -4,7 +4,8 @@ async function generateSignedUrl(videoKey) {
   const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
+    region: "us-east-2",
+    signatureVersion: "v4",
   });
 
   const params = {
